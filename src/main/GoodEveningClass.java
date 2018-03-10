68ba8f03c0d2c4c15fddd60e59fea80c952aa747
aseladamisaeva <aseladamisaeva@10.1.10.173>
2018-03-10 13:55:24 -0600

    changes are done by altynbek

--- src/main/GoodEveningClass.java
+++ src/main/GoodEveningClass.java
@@ -3,6 +3,7 @@ package main;
 public class GoodEveningClass {
 	public static void main(String[] args) {
 		System.out.println("Good Evening Class!!!");
+		System.out.println("altynbek ishenbaev");
 	}
 
 }
--- src/main/GoodMorningClass.java
+++ src/main/GoodMorningClass.java
@@ -4,6 +4,7 @@ public class GoodMorningClass {
 
 	public static void main(String[] args) {
 		System.out.println("Good Morning Class!!!");
+	    System.out.println("altynbek ishenbaev");
 
 	}
 
