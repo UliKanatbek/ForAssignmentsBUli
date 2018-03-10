409a5ccd1b6ca5ef6ab6c48a8f43f22013f9681c
aseladamisaeva <aseladamisaeva@10.1.10.173>
2018-03-10 14:11:48 -0600

    uli it is our first share

--- src/main/GoodEveningClass.java
+++ src/main/GoodEveningClass.java
@@ -4,6 +4,8 @@ public class GoodEveningClass {
 	public static void main(String[] args) {
 		System.out.println("Good Evening Class!!!");
 		System.out.println("altynbek ishenbaev");
+		System.out.println("uli is the best");
+		System.out.println("uli is kachok");
 	}
 
 }
--- src/main/GoodMorningClass.java
+++ src/main/GoodMorningClass.java
@@ -5,6 +5,8 @@ public class GoodMorningClass {
 	public static void main(String[] args) {
 		System.out.println("Good Morning Class!!!");
 	    System.out.println("altynbek ishenbaev");
+	    System.out.println("uli is the best");
+		System.out.println("uli is kachok");
 
 	}
 
